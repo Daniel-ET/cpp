23ec4df36801883305d325bb2feed556aa58c174
Dan Ettah <96924468+Daniel-ET@users.noreply.github.com>
2022-08-14 00:43:01 +0100

    Add files via upload

---
+++ part1/variables.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int x, y, z;
+
+    x = 20;
+    y = 9;
+    z = x + y;
+
+    string s = "twenty-nine";
+
+    cout << "use of variables in C++" << endl;
+    cout << "The result of x+y =" << z << endl;
+    cout << "twenty plus nine equals " << s << endl;
+
+    return 0;
+}
