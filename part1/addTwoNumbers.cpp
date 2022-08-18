a37784a513d7fa98510e847823e8d0aadeb8a520
Dan Ettah <96924468+Daniel-ET@users.noreply.github.com>
2022-08-18 18:50:43 +0100

    Add files via upload

---
+++ part1/addTwoNumbers.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x, y, sum;
+	cout << "Adding two numbers \n" << endl;
+
+	cout << "Insert first number: "<< endl;
+
+	cin >> x;
+	
+	cout << "insert second number: " << endl;
+
+	cin >> y;
+
+	sum = x +y;
+
+	cout << "The result of x + y is: " << sum << endl;
+
+	return 0;
+
+
+}
