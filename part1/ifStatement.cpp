4d7af8158a1c51fb34937d3e82f728268081d2e7
Dan Ettah <96924468+Daniel-ET@users.noreply.github.com>
2022-08-23 18:56:47 +0100

    Add files via upload

---
+++ part1/ifStatement.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x = 60;
+	cout << "If statement \n" << endl;
+
+	if (x > 35)
+	{
+		cout << "The value of x is greater than 35" << endl;
+	}
+
+
+	return 0;
+}
