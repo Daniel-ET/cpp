1ea4b9ce1428fdff67646d1b759fb12e88c305f8
Dan Ettah <96924468+Daniel-ET@users.noreply.github.com>
2022-08-23 20:59:01 +0100

    Add files via upload

---
+++ part2/ifElse.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x = 67;
+
+	cout << "if else statement \n" << endl;
+
+	if (x==8)
+	{
+		cout << "if Condition has been met" << endl;
+
+	}
+	else
+	{
+		cout << "if Condition has NOT been met" << endl;
+	}
+
+	return 0;
+}
