af841359c21e68d50663796a9147b62c823a263b
Dan Ettah <96924468+Daniel-ET@users.noreply.github.com>
2022-08-24 14:56:32 +0100

    Add files via upload

---
+++ part2/while.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x = 10;
+
+	cout << "while loop \n" << endl;
+
+	while (x <= 20)
+	{
+		cout << "The value of x is: " << x << endl;
+		x++;
+	}
+
+
+	return 0;
+}
+
