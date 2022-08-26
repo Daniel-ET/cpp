5eae0fb6dee77006539a273d469f3da70d1804e0
Dan Ettah <96924468+Daniel-ET@users.noreply.github.com>
2022-08-26 20:42:13 +0100

    Add files via upload

---
+++ part1/longlong.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    long b;
+    long long c;
+
+    cout << "Size of int = " << sizeof(a) << " bytes \n" << endl;
+    cout << "Size of long = " << sizeof(b) << " bytes\n" << endl;
+    cout << "Size of long long = " << sizeof(c) << " bytes\n" << endl;
+
+}
